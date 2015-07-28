4787d1f9b506c4bd6af3cecbd8c3cafe38230523
Gabe Fierro <gtfierro225@gmail.com>
2015-07-28 11:44:52 -0700

    GUH

--- react_src/dashboard.js
+++ react_src/dashboard.js
@@ -66,7 +66,7 @@ var Dashboard = React.createClass({
             function(data) {
                 var newstate = self.state.data;
                 _.each(data, function(obj) {
-                    if (obj.Readings == null) { return; }
+                    if (obj == null || obj.Readings == null || obj.uuid == null ) { return; }
                     if (newstate[obj.uuid] == null) { return; }
                     newstate[obj.uuid].latestValue = obj.Readings[obj.Readings.length-1][1];
                     newstate[obj.uuid].latestTime = moment.unix(obj.Readings[obj.Readings.length-1][0]);
